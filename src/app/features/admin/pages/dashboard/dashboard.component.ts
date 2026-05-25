e520e7daeaad05e5c51b52a54575830dfe3ea41c
MnnaMhmed <mm5261@fayoum.edu.eg>
2026-05-25 10:14:39 +0300

    fix build

--- src/app/features/admin/pages/dashboard/dashboard.component.ts
+++ src/app/features/admin/pages/dashboard/dashboard.component.ts
@@ -1,5 +1,4 @@
 import { Component , AfterViewInit} from '@angular/core';
-import Chart from 'chart.js/auto';
 @Component({
   selector: 'app-dashboard',
   standalone: true,
@@ -7,20 +6,8 @@ import Chart from 'chart.js/auto';
   templateUrl: './dashboard.component.html',
   styleUrl: './dashboard.component.scss'
 })
-export class DashboardComponent implements AfterViewInit {
-ngAfterViewInit() {
-    new Chart('myChart', {
-      type: 'line',
-      data: {
-        labels: ['Jan', 'Feb', 'Mar', 'Apr'],
-        datasets: [{
-          label: 'Orders',
-          data: [10, 20, 15, 30],
-          borderColor: 'orange'
-        }]
-      }
-    });
-  }
+export class DashboardComponent   {
+
 }
 
 
